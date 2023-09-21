16f6c3cc87c21ca6bb9b1567906153f97c91c05d
ldex <laurent.duveau+git@gmail.com>
2023-09-21 11:05:40 -0400

    Products Module

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 import { Product } from 'src/app/models/product.interface';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -10,20 +11,50 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent {
 
   title: string = 'Products';
-  products: Product[];
+  //products: Product[];
+  products$: Observable<Product[]>;
+  productsNumber: number = 0;
   selectedProduct: Product;
+
+  // Pagination
+  pageSize = 5;
+  start = 0;
+  end = this.pageSize;
+  pageNumber = 1;
+
+  previousPage(): void {
+    this.start -= this.pageSize;
+    this.end -= this.pageSize;
+    this.pageNumber--;
+    this.selectedProduct = null;
+  }
+
+  nextPage(): void {
+    this.start += this.pageSize;
+    this.end += this.pageSize;
+    this.pageNumber++;
+    this.selectedProduct = null;
+  }
+
 
   onSelect(product: Product): void {
     this.selectedProduct = product;
   }
 
   constructor(private productService: ProductService) {
-    this
-      .productService
-      .products$
-      .subscribe(
-        data => this.products = data
-      )
+    this.products$ = this
+                      .productService
+                      .products$
+                      .pipe(
+                        tap(products => this.productsNumber = products.length)
+                      );
+
+    // this
+    //   .productService
+    //   .products$
+    //   .subscribe(
+    //     data => this.products = data
+    //   )
   }
 
 }
