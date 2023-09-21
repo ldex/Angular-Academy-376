df188cfa7c3f671b85768f7304a62267dbc04a82
ldex <laurent.duveau+git@gmail.com>
2023-09-21 15:52:54 -0400

    Product Delete

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/models/product.interface';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -12,9 +12,23 @@ export class ProductDetailComponent {
 
   @Input() product: Product;
 
+  deleteProduct() {
+    this
+      .productService
+      .deleteProduct(this.product.id)
+      .subscribe(
+        () => {
+          console.log('Product was deleted on the server.');
+          this.productService.initProducts();
+          this.router.navigateByUrl('/products');
+        }
+      )
+  }
+
   constructor(
     private activatedRoute: ActivatedRoute,
-    private productService: ProductService
+    private productService: ProductService,
+    private router: Router
   ) {
     let id = this.activatedRoute.snapshot.params["id"];
 
